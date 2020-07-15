496898a14dc86732ac9c296ed5110211cd2eb386
= <=>
2020-07-15 17:37:50 -0300

    implementação do CRUD de usuário

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "login": "John" }
 
+// const name = req.query.name ? req.query.name : 'visitante' ;
+// const id = req.params.id ? req.params.id : 0;
+
 const users = ['Yago', 'João', 'Fernando'];
 
-server.get('/users/:id', (req, res) => {
-    const name = req.query.name ? req.query.name : 'visitante' ;
-    // const id = req.params.id ? req.params.id : 0;
-    const { id } = req.params;
+server.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+    return res.json(users[index]);
+});
 
-    return res.json(users[id]);
+server.get('/users', (req, res) => {
+    return res.json(users);
+});
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+    users.push(name);
+    return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    users[index] = name;
+    return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params;
+    users.pop(index);
+    return res.json(users);
 });
 
 server.listen(5000);
